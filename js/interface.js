a14ee2e777166408c5783ea3ccc35cfec5f15e4e
Tony Wu <tonytlwu@users.noreply.github.com>
2024-01-30 22:48:39 +0800

    ID-3904 Updates update options to use HTML tags

--- js/interface.js
+++ js/interface.js
@@ -7,15 +7,15 @@ Fliplet.Widget.generateInterface({
       options: [
         {
           value: 'none',
-          label: 'No Update - Updates are not silently applied and users won\'t see the changes until they load the list for the next time.'
+          label: '<strong>No Update</strong> - Updates are not silently applied and users won\'t see the changes until they load the list for the next time.'
         },
         {
           value: 'informed',
-          label: 'Informed Update - Users are informed if an update is available. When the user chooses to apply it, changes are applied in-situ, i.e. without a complete reload.'
+          label: '<strong>Informed Update</strong> - Users are informed if an update is available. When the user chooses to apply it, changes are applied in-situ, i.e. without a complete reload.'
         },
         {
           value: 'live',
-          label: 'Real-time Update - Updates are automatically applied when they are available. Detail view can be directly loaded (via query parameter) without loading the list first.'
+          label: '<strong>Real-time Update</strong> - Updates are automatically applied when they are available. Detail view can be directly loaded (via query parameter) without loading the list first.'
         }
       ]
     },
